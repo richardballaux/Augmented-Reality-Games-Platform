15c6995cf2313d0fc859a311fd4eb239cac88b1d
Viktor Deturck <vdeturck@olin.edu>
2018-11-14 12:05:49 -0500

    SetColorTest now usuable as a function

--- SetColorTest.py
+++ SetColorTest.py
@@ -1,39 +1,44 @@
-"""this file is the first stage to make a function in the arcade where you can choose the color of you controller"""
 import cv2
 import numpy as np
 import time
 
-cam= cv2.VideoCapture(0)
-resolution = [340,220]
-colorRange = [255,255,255,0,0,0]
-rectangle = [(int(resolution[0]/2-resolution[0]/15),int(resolution[1]/2-resolution[1]/15)),(int(resolution[0]/2+resolution[0]/15),int(resolution[1]/2+resolution[1]/15))]
-startTime = time.time()
+def calibrateColor(resolution):
+    """
+    Starts a camera screen and records the HSV values inside the drawn rectangle (ROI)
 
-
-def getValues():
-    while True:
+    Takes a resolution to draw the screen
+    Returns a list of the min and max values of the recorded HSV values
+    """
+    cam= cv2.VideoCapture(0)
+    colorRange = [255,255,255,0,0,0]
+    rectangle = [(int(resolution[0]/2-resolution[0]/25),int(resolution[1]/2-resolution[1]/25)),(int(resolution[0]/2+resolution[0]/25),int(resolution[1]/2+resolution[1]/25))]
+    startTime = time.time() # Saves the time the program started
+    running = True
+    font = cv2.FONT_HERSHEY_SIMPLEX  # font for the timer
+    while running == True:
         ret, img=cam.read()
-        img = cv2.resize(img,(resolution[0],resolution[1]))
-        img = cv2.flip(img, 1)
-        cv2.rectangle(img, rectangle[0],rectangle[1],(0,0,255),2)
-        roi = img[rectangle[0][1]:rectangle[1][1],rectangle[0][0]:rectangle[1][0]]
+        img = cv2.resize(img,(resolution[0],resolution[1]))  # Resize the camera view to the desired resolution
+        img = cv2.flip(img, 1)                               # Flip it
+        cv2.rectangle(img, rectangle[0],rectangle[1],(0,0,255),2)  # Draw a rectangle for the scanning area
+        roi = img[rectangle[0][1]:rectangle[1][1],rectangle[0][0]:rectangle[1][0]] # Defines the region of interest ROI
         hsvRoi = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
-        #print('min H = {}, min S = {}, min V = {}; max H = {}, max S = {}, max V = {}'.format(hsvRoi[:,:,0].min(), hsvRoi[:,:,1].min(), hsvRoi[:,:,2].min(), hsvRoi[:,:,0].max(), hsvRoi[:,:,1].max(), hsvRoi[:,:,2].max()))
-        cv2.imshow("cam",img)
-        cv2.waitKey(10)
-        #print(time.time()-startTime)
-        if time.time()-startTime>3 and time.time()-startTime<5:          # Between 3 and 5 secs
+        cv2.putText(img,str(int(time.time()-startTime)),(10,20), font, 1,(255,255,255),2)  # Displays the time
+
+        if time.time()-startTime>3 and time.time()-startTime<6:          # Between 3 and 5 secs
             for i in range(3):                   # Save the  values of HSV
-                print(hsvRoi[:,:,i].min())
                 if hsvRoi[:,:,i].min() < colorRange[i]:
-                    print("black")
                     colorRange[i] = hsvRoi[:,:,i].min()
             for i in range(3):                 # Save the max values of HSV
                 if hsvRoi[:,:,i].max() > colorRange[i+3]:
-                    print("check")
                     colorRange[i+3] = hsvRoi[:,:,i].max()
-            print(colorRange)
+        if  time.time() - startTime > 5.5: # Breaks the while loop and the program
+            running = False
+            cv2.destroyAllWindows()
+        cv2.imshow("cam",img)
+        cv2.waitKey(10)
     return colorRange
 
-list = getValues()
-print(list)
+if __name__ == '__main__':
+    res = [340,220]
+    range = calibrateColor(res)
+    print(range)
