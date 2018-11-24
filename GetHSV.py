5313777522b8cf079e9fa052c9c7bab537ffdcd6
Viktor Deturck <vdeturck@olin.edu>
2018-11-24 18:34:59 -0500

    objectionrec enhanced

--- GetHSV.py
+++ GetHSV.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import time
 
 def calibrateColor(resolution):
     """
@@ -10,11 +9,8 @@ def calibrateColor(resolution):
     Returns a list of the min and max values of the recorded HSV values
     """
     cam= cv2.VideoCapture(0)
-    colorRange = [255,255,255,0,0,0]
     rectangle = [(int(resolution[0]/2-resolution[0]/25),int(resolution[1]/2-resolution[1]/25)),(int(resolution[0]/2+resolution[0]/25),int(resolution[1]/2+resolution[1]/25))]
-    startTime = time.time() # Saves the time the program started
     running = True
-    font = cv2.FONT_HERSHEY_SIMPLEX  # font for the timer
     while running == True:
         ret, img=cam.read()
         img = cv2.resize(img,(resolution[0],resolution[1]))  # Resize the camera view to the desired resolution
@@ -22,15 +18,20 @@ def calibrateColor(resolution):
         cv2.rectangle(img, rectangle[0],rectangle[1],(0,0,255),2)  # Draw a rectangle for the scanning area
         roi = img[rectangle[0][1]:rectangle[1][1],rectangle[0][0]:rectangle[1][0]] # Defines the region of interest ROI
         hsvRoi = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
-        cv2.putText(img,str(int(time.time()-startTime)),(10,20), font, 1,(255,255,255),2)  # Displays the time
+        # colorRange = hsvRoi[:,:,]
         print(hsvRoi)
-        if time.time()-startTime>3 and time.time()-startTime<6:          # Between 3 and 5 secs
-            for i in range(3):                   # Save the  values of HSV
-                if hsvRoi[:,:,i].min() < colorRange[i]:
-                    colorRange[i] = hsvRoi[:,:,i].min()
-            for i in range(3):                 # Save the max values of HSV
-                if hsvRoi[:,:,i].max() > colorRange[i+3]:
-                    colorRange[i+3] = hsvRoi[:,:,i].max()
+        # if time.time()-startTime>3 and time.time()-startTime<6:          # Between 3 and 5 secs
+        #     for i in range(3):                   # Save the  values of HSV
+        #         if hsvRoi[:,:,i].min() < colorRange[i]:
+        #             colorRange[i] = hsvRoi[:,:,i].min()
+        #     for i in range(3):                 # Save the max values of HSV
+        #         if hsvRoi[:,:,i].max() > colorRange[i+3]:
+        #             colorRange[i+3] = hsvRoi[:,:,i].max()
+        # if  time.time() - startTime > 5.5: # Breaks the while loop and the program
+        #     running = False
+        #     cv2.destroyAllWindows()
+        #print(colorRange)
+        
         cv2.imshow("cam",img)
         cv2.waitKey(10)
     return colorRange
---
+++ OriginalObjRec.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import SetColorTest as sct
+
+bounds = np.asarray(sct.calibrateColor([340,220]))
+lowerBound=bounds[0:3]
+upperBound=bounds[3:6]
+
+cam= cv2.VideoCapture(0)
+kernelOpen=np.ones((5,5))
+kernelClose=np.ones((20,20))
+
+
+while True:
+    ret, img=cam.read()
+    img=cv2.resize(img,(340,220))
+
+    #convert BGR to HSV
+    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    # create the Mask
+    mask=cv2.inRange(imgHSV,lowerBound,upperBound)
+    #morphology
+    maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+    maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+
+    maskFinal=maskClose
+    im,conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+    cv2.drawContours(img,conts,-1,(255,0,0),3)
+    for i in range(len(conts)):
+        x,y,w,h=cv2.boundingRect(conts[i])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255), 2)
+    cv2.imshow("cam",img)
+    cv2.waitKey(10)
